79309a3b8183a1be91ab7db742926c9299ad2ebd
itzxko <20210495m.bejerano.joshua.bscs@gmail.com>
2024-07-03 21:21:19 +0800

    commit

--- src/Data/Products.js
+++ src/Data/Products.js
@@ -17,6 +17,60 @@ const Products = [
     price: 7380,
     img: "https://images.unsplash.com/21/mac-glasses.JPG?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
+  {
+    id: "4",
+    name: "ADDE Wooden Study Chair",
+    price: 7380,
+    img: "https://images.unsplash.com/photo-1524758631624-e2822e304c36?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "5",
+    name: "FLINTAN Office Chair",
+    price: 7380,
+    img: "https://images.unsplash.com/photo-1489269637500-aa0e75768394?q=80&w=1141&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "6",
+    name: "MILLBURGET Swivel Chair",
+    price: 7380,
+    img: "https://images.unsplash.com/photo-1612372606404-0ab33e7187ee?q=80&w=978&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "7",
+    name: "IVAR Cabinet with Doors",
+    price: 7380,
+    img: "https://plus.unsplash.com/premium_photo-1683121820976-d90c558311bd?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "8",
+    name: "BAGEBO Bathroom Organizer",
+    price: 7380,
+    img: "https://images.unsplash.com/photo-1595515106969-1ce29566ff1c?q=80&w=1176&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "9",
+    name: "LAGKAPTEN Wooden Table",
+    price: 7380,
+    img: "https://images.unsplash.com/photo-1565791380713-1756b9a05343?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "10",
+    name: "BLASVERK Kitchen Chandelier",
+    price: 7380,
+    img: "https://images.unsplash.com/photo-1540932239986-30128078f3c5?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "11",
+    name: "TAGARP Staircase Lamp",
+    price: 7380,
+    img: "https://images.unsplash.com/photo-1567459045800-4d77c81fc3f5?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "12",
+    name: "ARTISAN Felt Deskpad",
+    price: 7380,
+    img: "https://images.unsplash.com/flagged/photo-1576697011479-349e2a52bdf6?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
 ];
 
 export { Products };
--- src/Scripts/Checkout.js
+++ src/Scripts/Checkout.js
@@ -1,7 +1,8 @@
 import { Products } from "../Data/Products.js";
-import { cart, removeProduct } from "../Data/Cart.js";
+import { cart, cartCount, removeProduct, ifEmptyCart } from "../Data/Cart.js";
 
 const container = document.getElementById("cart-container");
+const priceContainer = document.getElementById("total-price");
 
 let html = ``;
 cart.forEach((items) => {
@@ -12,64 +13,89 @@ cart.forEach((items) => {
   Products.forEach((product) => {
     if (product.id === productId) {
       matchingProduct = product;
+
+      // console.log(`Price: ${matchingProduct.price}, Qty: ${items.quantity}`);
+      // totalPrice += parseInt(matchingProduct.price) * parseInt(items.quantity);
+      // priceContainer.innerText = totalPrice;
     }
   });
 
   html += `
-      <div
-          class="relative w-full flex flex-row items-center justify-center h-full p-4" id="cart-product-${matchingProduct.id}"
-        >
-          <div
-            class="relative h-[160px] w-full flex items-center justify-center rounded-md overflow-hidden cursor-pointer group"
+                <div
+            class="relative w-full flex flex-row items-center justify-center h-full p-4"
+            id="cart-product-${matchingProduct.id}"
           >
-            <img
-              src="${matchingProduct.img}"
-              alt=""
-              class="object-cover w-full h-full hover:scale-110 transition-transform duration-500"
-            />
             <div
-              class="absolute top-2 left-2 hidden group-hover:block cursor-pointer"
+              class="relative h-[160px] w-full flex items-center justify-center rounded-md overflow-hidden cursor-pointer group"
             >
-              <div class="remove-button" id="" data-id="${matchingProduct.id}">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="size-4 text-white shadow-xl"
+              <img
+                src="${matchingProduct.img}"
+                alt=""
+                class="object-cover w-full h-full duration-500 hover:scale-105 transition-transform"
+              />
+              <div
+                class="absolute top-2 left-2 hidden group-hover:block cursor-pointer"
+              >
+                <div
+                  class="remove-button "
+                  id=""
+                  data-id="${matchingProduct.id}"
                 >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                  />
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-4 "
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6 18 18 6M6 6l12 12"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div
-            class="absolute bottom-8 right-[-12px] flex flex-col items-start justify-center py-2 px-4 w-2/4 bg-[#2f2f2f] text-white shadow-xl rounded-md"
-          >
-            <p class="text-sm font-semibold w-full truncate">
-              ${matchingProduct.name}
-            </p>
-            <p class="text-xs font-normal w-full truncate">
-              PHP ${matchingProduct.price}
-            </p>
-            <p class="text-xs font-normal w-full truncate">
-              Quantity: ${items.quantity}
-            </p>
+            <div
+              class="absolute bottom-8 right-[-12px] flex flex-col items-start justify-center py-2 px-4 w-2/4 bg-[#2f2f2f] text-white shadow-xl rounded-md"
+            >
+              <p class="text-sm font-semibold w-full truncate">
+                ${matchingProduct.name}
+              </p>
+              <p class="text-xs font-normal w-full truncate">
+                PHP ${matchingProduct.price}
+              </p>
+              <p class="text-xs font-normal w-full truncate">
+                Quantity: ${items.quantity}
+              </p>
+            </div>
           </div>
-        </div>
     `;
 });
 container.innerHTML = html;
 
 const remove = document.querySelectorAll(".remove-button");
 
-function removeItem() {}
+function calculateTotalPrice() {
+  let totalPrice = 0;
+
+  cart.forEach((items) => {
+    let matchingProduct;
+
+    Products.forEach((product) => {
+      if (product.id === items.id) {
+        matchingProduct = product;
+
+        totalPrice +=
+          parseInt(matchingProduct.price) * parseInt(items.quantity);
+      }
+    });
+  });
+  priceContainer.innerText = `PHP ${totalPrice}`;
+}
 
 remove.forEach((items) => {
   items.addEventListener("click", () => {
@@ -78,5 +104,10 @@ remove.forEach((items) => {
     console.log(productId);
     const parent = document.getElementById(`cart-product-${productId}`);
     parent.remove();
+    cartCount();
+    calculateTotalPrice();
+    ifEmptyCart();
   });
 });
+calculateTotalPrice();
+ifEmptyCart();
